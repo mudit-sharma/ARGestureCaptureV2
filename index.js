a1fe1b9e11e46bf98a4fe41f2879792159963aa7
Len <lennemo09@gmail.com>
2022-01-06 12:41:23 +1100

    Added basic static pages.

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
+const https = require('https');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
+
+//site variables
+const appInfo = {
+    title: '3D Hand Gestures Analysis', 
+    description:'WEB API for detecting hand gestures',
+};
+
+const pageInfo = [
+    {key: 'index', value: ''},
+    {key: 'mediapipe', value: 'MediaPipe'},
+    {key: 'contact', value: 'Contact Us'},
+];
+
+// Available Gesture types(actions).
+const positionList = [
+    "Select Range",
+    "Select Lasso", 
+    "Select Cluster", 
+    "Select Single Point", 
+    "Select Axis", 
+    "Multi-Select", 
+    "Zoom", 
+    "Pan", 
+    "Rotate", 
+    "Filter", 
+    "Highlight", 
+    "Save View", 
+    "Export Data"
+];
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Example app listening at ${port}`);
+    console.log(`App listening at http://localhost:${port}`);
+    console.log('Press Ctrl+C to quit.');
 });
